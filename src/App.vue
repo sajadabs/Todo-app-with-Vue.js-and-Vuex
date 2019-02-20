1a28ba0afbfc73ef839a7403f6dcce3f384fbeb2
Sajad Abasi <sab3053@gmail.com>
2019-02-20 18:18:51 +0330

    add Date component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app" class="container">
+    <Date></Date>
     <CompletedTodos></CompletedTodos>
     <GetTodo></GetTodo>
     <CurrentTodos></CurrentTodos>
   </div>
 </template>
 <script>
+    import Date from './components/Date.vue'
     import GetTodo from './components/GetTodo.vue'
     import CurrentTodos from './components/CurrentTodos.vue'
     import CompletedTodos from './components/CompletedTodos.vue'
     export default {
         components: {
+            Date,
             GetTodo,
             CurrentTodos,
             CompletedTodos
