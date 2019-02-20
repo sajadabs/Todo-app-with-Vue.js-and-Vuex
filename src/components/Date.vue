9420b9e042fafa3d50d9c4f925f2f7db5ee9a300
Sajad Abasi <sab3053@gmail.com>
2019-02-20 18:18:58 +0330

    add Date component

---
+++ src/components/Date.vue
@@ -0,0 +1,7 @@
+<template>
+    <div id="date" class="container">
+        <span>{{ new Date() | moment("jDD/jMM/jYYYY") }}</span>
+        <i class="la la-user"></i>
+        <button class="btn btn-danger"></button>
+    </div>
+</template>
