6366405dc83e0dcd9c5b13181dd93e3efb428d1b
Sajad Abasi <sab3053@gmail.com>
2019-02-21 17:03:55 +0330

    add bootstrap vue library

--- src/main.js
+++ src/main.js
@@ -4,7 +4,11 @@ import Vue from 'vue'
 import App from './App'
 import store from './store/store'
 import './assets/css/app.css'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+Vue.use(BootstrapVue)
 Vue.use(require('vue-jalali-moment'))
 Vue.config.productionTip = false
 
