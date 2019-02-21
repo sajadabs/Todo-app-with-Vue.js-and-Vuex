a05dbfe8b012d6146c7aac12f28ba6be04e4e6a3
Sajad Abasi <sab3053@gmail.com>
2019-02-21 15:06:06 +0330

    bug fix

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app" class="container">
+    <Date></Date>
+    <CompletedTodos></CompletedTodos>
+    <GetTodo></GetTodo>
+    <CurrentTodos></CurrentTodos>
+  </div>
+</template>
+<script>
+    import Date from './components/Date.vue'
+    import GetTodo from './components/GetTodo.vue'
+    import CurrentTodos from './components/CurrentTodos.vue'
+    import CompletedTodos from './components/CompletedTodos.vue'
+    export default {
+        components: {
+            Date,
+            GetTodo,
+            CurrentTodos,
+            CompletedTodos
+        }
+
+    }
+</script>
+<style>
+  body {
+    font-family: Helvetica, sans-serif;
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,16 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import store from './store/store'
+import './assets/css/app.css'
+
+Vue.use(require('vue-jalali-moment'))
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+    el: '#app',
+    store,
+    render: h => h(App)
+})
