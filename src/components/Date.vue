425bbfd889e0542f00705e42de1e65a123377a8b
Sajad Abasi <sab3053@gmail.com>
2019-02-23 12:44:03 +0330

    add line icon components.

--- src/components/Date.vue
+++ src/components/Date.vue
@@ -1,7 +1,14 @@
 <template>
     <div id="date" class="container">
+        تاریخ امروز :
         <span>{{ new Date() | moment("jDD/jMM/jYYYY") }}</span>
-        <i class="la la-user"></i>
+        <line-icon
+                icon="plus"
+                :rotate="90"
+                size="small"
+                color="mediumseagreen"
+                no-svg
+        />
         <button class="btn btn-danger"></button>
     </div>
 </template>
