718afb08b0fdf10fe97b0e9b0a6a3cdddf7fb7bc
Sajad Abasi <sab3053@gmail.com>
2019-02-24 20:02:26 +0330

    change components style.

---
+++ src/components/TodoForm.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="todo-form">
+        <b-modal
+                v-model="modal"
+                title="افزودن کار جدید"
+                @ok="addTodo"
+                ok-variant="success"
+                cancel-variant="danger"
+                ok-title="تایید"
+                cancel-title="انصراف"
+                hide-header-close
+
+        >
+            <input class="form-control"
+                   :value="newTodo"
+                   @change="getTodo"
+                   placeholder="متن کار جدید ..."
+            >
+        </b-modal>
+    </div>
+</template>
+<script>
+    export default {
+        data: function () {
+            return {
+                modal: true
+            }
+        },
+        props:[
+            'todo'
+        ],
+
+    }
+</script>
