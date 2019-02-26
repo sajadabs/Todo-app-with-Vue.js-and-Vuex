6fb57bf82a3bf8e623fabe222b7c02e98e510d6f
Sajad Abasi <sab3053@gmail.com>
2019-02-26 17:01:16 +0330

    hotfix

--- src/components/TodoForm.vue
+++ src/components/TodoForm.vue
@@ -2,33 +2,48 @@
     <div id="todo-form">
         <b-modal
                 v-model="modal"
-                title="افزودن کار جدید"
-                @ok="addTodo"
-                ok-variant="success"
-                cancel-variant="danger"
-                ok-title="تایید"
-                cancel-title="انصراف"
-                hide-header-close
-
+                hide-header
+                hide-footer
+                no-close-on-esc
+                no-close-on-backdrop
         >
-            <input class="form-control"
+            <h5 class="text-right">
+                <div v-if="showEdit">ویرایش کار</div>
+                <div v-else>افزودن کار جدید</div>
+            </h5>
+            <hr>
+
+            <input class="form-control mb-3"
                    :value="newTodo"
                    @change="getTodo"
                    placeholder="متن کار جدید ..."
             >
+
+            <b-button variant="danger" @click="cancelForm()">
+                انصراف
+            </b-button>
+
+            <b-button variant="success" @click="showEdit ? submitEdit(todo) : addTodo()">
+                <div v-if="showEdit">ویرایش</div>
+                <div v-else>افزودن</div>
+            </b-button>
+
         </b-modal>
     </div>
 </template>
 <script>
+    import mixin from '../mixin/mixin.js'
+
     export default {
+        mixins: [mixin],
         data: function () {
             return {
                 modal: true
             }
         },
-        props:[
-            'todo'
-        ],
+        props: [
+            'todo', 'showEdit'
+        ]
 
     }
 </script>
